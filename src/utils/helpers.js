41a0e3cb4c2644e4827816ead9c542b7ad361a5c
joaquinOEF <92546500+joaquinOEF@users.noreply.github.com>
2024-12-13 14:07:52 +0000

    Assistant checkpoint: Add type check to toTitleCase function
    Assistant generated file changes:
    - src/utils/helpers.js: Add type check to toTitleCase function
    
    ---
    
    User prompt:
    
    I still get this error when clicking the adaptation tab

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,41 +1,28 @@
+
 export const ADAPTATION = 'adaptation';
 export const MITIGATION = 'mitigation';
 
 export function getReductionPotential(action) {
-    // if (!action.Sector) return ''
-    // let field;
-    // switch (action.Sector) {
-    //     case 'Stationary Energy':
-    //         field = 'energy';
-    //         break;
-    //
-    //     case "Transport":
-    //         field = 'transportation';
-    //         break;
-    //
-    //     case "Waste":
-    //         field = 'waste'
-    //         break;
-    // }
-    // const fullField = `GHGReductionPotential.${field}`
     if (!action.GHGReductionPotential) return ''
     return Object.values(action.GHGReductionPotential).find((value) => {
         return !!value
     })
 }
 
-export const toTitleCase = str => str.replace(
-    /\w\S*/g,
-    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
-);
-export const isAdaptation = type => type?.toLowerCase  && type.toLowerCase() === ADAPTATION.toLowerCase();
+export const toTitleCase = str => {
+    if (typeof str !== 'string') return '';
+    return str.replace(
+        /\w\S*/g,
+        text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+};
 
-// Function to get nested property value
+export const isAdaptation = type => type?.toLowerCase && type.toLowerCase() === ADAPTATION.toLowerCase();
+
 const getNestedValue = (obj, path) => {
     return path.split('.').reduce((acc, part) => acc && acc[part], obj);
 };
 
-// Prepare CSV data for export
 export const prepareCsvData = (tableData, columns) => {
     return tableData.map(row =>
         columns.reduce((acc, column) => {
