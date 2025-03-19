cebc943c04f13514861f650afddcfaf010b6d9c7
Milan Gruner <milangruner@gmail.com>
2025-03-19 13:28:41 +0100

    feat: add ADAPTA_BRASIL_API constant

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,10 @@
 export const CITIES = [
-    {value: 'Camaçari', label: "Camaçari", locode: "BRCCI"},
-    {value: 'Caxias do Sul', label: "Caxias do Sul", locode: "BRCXL"},
-    {value: 'Corumba', label: "Corumba", locode: "BRCMG"},
-    {value: 'Rio Branco', label: "Rio Branco", locode: "BRRBR"},
-    {value: 'Serra', label: "Serra", locode: 'BRSER'},
+  { value: "Camaçari", label: "Camaçari", locode: "BRCCI" },
+  { value: "Caxias do Sul", label: "Caxias do Sul", locode: "BRCXL" },
+  { value: "Corumba", label: "Corumba", locode: "BRCMG" },
+  { value: "Rio Branco", label: "Rio Branco", locode: "BRRBR" },
+  { value: "Serra", label: "Serra", locode: "BRSER" },
 ];
+
+export const ADAPTA_BRASIL_API = "https://adapta-brasil-api.replit.app";
+
