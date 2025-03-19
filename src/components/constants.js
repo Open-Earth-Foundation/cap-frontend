53fe4c1545a1c971fec6de1e97e1c6314f7f752d
piotrnowakowski <piotr98nowakowski@gmail.com>
2025-03-19 15:29:56 +0100

    added city missing to constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -4,6 +4,7 @@ export const CITIES = [
   { value: "Corumba", label: "Corumba", locode: "BRCMG" },
   { value: "Rio Branco", label: "Rio Branco", locode: "BRRBR" },
   { value: "Serra", label: "Serra", locode: "BRSER" },
+  { value: "Maranguape", label: "Maranguape", locode: "BRMGE" }
 ];
 
 export const ADAPTA_BRASIL_API = "https://adapta-brasil-api.replit.app";
