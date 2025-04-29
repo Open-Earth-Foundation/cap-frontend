f0ac326e8fec149928bca53b70fe01f594637e01
Marina Olivella <marina@openearth.org>
2025-04-29 20:35:20 -0300

    view more or less

--- src/components/CityData.jsx
+++ src/components/CityData.jsx
@@ -4,10 +4,13 @@ import { useTranslation } from "react-i18next";
 import { HeadlineSmall } from "./Texts/Headline";
 import { LessView } from "./LessView";
 import { MoreView } from "./MoreView";
-import { TitleLarge } from "./Texts/Title";
+import { ButtonMedium } from "./Texts/Button";
+import { KeyboardArrowUp, KeyboardArrowDown } from "@mui/icons-material";
+
 const CityData = ({ selectedLocode }) => {
     const { t } = useTranslation();
     const [cityData, setCityData] = useState({});
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const fetchCityData = async () => {
@@ -17,11 +20,24 @@ const CityData = ({ selectedLocode }) => {
         };
         fetchCityData();
     }, [selectedLocode]);
+
+    const toggleView = () => {
+        setShowMore(!showMore);
+    };
+
     return (
         <>
             <HeadlineSmall>{cityData?.name}</HeadlineSmall>
             <LessView cityData={cityData} />
-            <MoreView cityData={cityData} />
+            {showMore ? <MoreView cityData={cityData} /> : null}
+            <div className="mt-4">
+                <div onClick={toggleView} className="flex justify-center items-center gap-2">
+                    {showMore ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                    <ButtonMedium>
+                        {showMore ? t('viewLess') : t('viewMore')}
+                    </ButtonMedium>
+                </div>
+            </div>
         </>
     );
 };
